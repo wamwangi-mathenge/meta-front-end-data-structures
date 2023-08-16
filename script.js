67ba50cd834457cfb1a28d441125e2a999f5e110
Brian <wamwangimathenge@gmail.com>
2023-08-16 14:31:25 +0300

    Code examples of a filter() method
    This commit demonstrates how to use the filter() method on an array.
    The method filters your arrays based on a specific test. Those array
    items that pass the test are returned.

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ const veggies = ["onion", "garlic", "ginger", "tomato"]
 veggies.forEach(function(veggie, index){
     console.log(`${index} ${veggie}`)
 })
+
+console.log("-----------------------------")
+console.log("-----------------------------")
+
+// The filter() method
+// Example 1
+const nums = [0,10,20,30,40,50]
+const filteredNums = nums.filter(function(num){
+    return num > 20
+})
+console.log(filteredNums)
+
+console.log("------------------------------")
+
+// Example 2
+// Find Even Numbers: Filter out all the even numbers from an array of integers.
+const numms = [1,2,3,4,5,6,7,8,9]
+const evenNumbers = numms.filter(function(numm){
+    return numm % 2 == 0
+})
+console.log(evenNumbers)
